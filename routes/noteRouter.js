d083a619a45fe419df6adf552550bc5af8413b40
Ruveendra Dewshan <ruveendradeewshan@gmail.com>
2022-08-07 21:16:57 +0530

    Backend - Note Functionalities
    -Get All notes
    -Get Note
    -Create Note
    -Update Note
    -Delete Note

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+auth = require("../middleware/auth");
+const noteCtrl = require("../controllers/noteCtrl");
 
-router.route("/").get((req, res) =>
-  res.json({
-    msg: "Test Notes",
-  })
-);
+// prettier-ignore
+router.route("/")
+    .get(auth, noteCtrl.getNotes)
+    .post(auth, noteCtrl.createNote);
+
+router
+  .route("/:id")
+  .get(auth, noteCtrl.getNote)
+  .put(auth, noteCtrl.updateNote)
+  .delete(auth, noteCtrl.deleteNote);
 
 module.exports = router;
