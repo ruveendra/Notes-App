8d8848e2f05d00a7f010204a70668eed787e4bbf
Ruveendra Dewshan <ruveendradeewshan@gmail.com>
2022-08-08 01:24:05 +0530

    Front End - Login Page
    - JWT authentication implemented successfully
    - Tested login and registration endpoints
    - Added styles

---
+++ client/src/components/Notes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+ function Notes({setIsLogin}) {
+  return (
+    
+      <div className="notes-page">
+        Notes
+      </div>
+    
+  );
+}
+
+export default Notes
